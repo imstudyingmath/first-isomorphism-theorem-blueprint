820b77b2d3c237b76a43c72c48d6fa2ed801ac75
imstudyingmath <simearth@gmail.com>
2026-04-03 20:49:13 +0900

    Update Example.lean

--- Project/Example.lean
+++ Project/Example.lean
@@ -4,5 +4,6 @@ open QuotientGroup
 
 variable {G H : Type*} [Group G] [Group H] (φ : G →* H)
 
+/-- Define and link the First Isomorphism Theorem using existing definitions from Mathlib. -/
 noncomputable def my_first_iso : G ⧸ φ.ker ≃* φ.range :=
   quotientKerEquivRange φ
